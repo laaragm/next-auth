a1ed2a947ebaf414fd926ff4e52cc1a54c2dd3be
Lara <laragalvanim@gmail.com>
2022-01-18 18:42:00 -0300

    Recover token using getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import { GetServerSideProps } from "next";
 import { FormEvent, useContext, useState } from "react";
+import { parseCookies } from "nookies";
 
 import { AuthContext } from "../contexts/AuthContext";
 import styles from "../styles/Home.module.css";
+import { redirect } from "next/dist/server/api-utils";
 
 export default function Home() {
     const [email, setEmail] = useState("");
@@ -33,3 +36,20 @@ export default function Home() {
         </form>
     );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const cookies = parseCookies(ctx);
+
+    if (!cookies["nextAuth.token"]) {
+        return {
+            redirect: {
+                destination: "/dashboard",
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {},
+    };
+};
